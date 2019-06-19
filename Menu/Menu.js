4ddde929c29444096f9d045baf35e3132bcb666e
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-19 14:17:36 -0700

    add outside menu click handling

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,8 +1,10 @@
+let menuOpen = false;
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open');
   // Toggle animation slider
   slideAnim.reversed(!slideAnim.reversed());
+  menuOpen = !menuOpen;
 };
 
 // Start Here: Create a reference to the ".menu" class
@@ -12,7 +14,20 @@ let slideAnim = TweenMax.to(menu, 1, { left: '0' }).reverse();
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu());
+menuButton.addEventListener('click', event => {
+  event.stopPropagation();
+  toggleMenu();
+});
+
+// Animation for menu button sliding in on load
 window.addEventListener('load', () =>
   TweenMax.to(menuButton, 1, { left: '25px' })
 );
+
+// Handle sliding menu back in if the menu is open and the user clicks something else
+document.querySelector('html').addEventListener('click', () => {
+  if (menuOpen) {
+    toggleMenu();
+  }
+});
+menu.addEventListener('click', event => event.stopPropagation());
