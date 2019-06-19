5306e757205add70965f35f4977fa6c4b0bbeb61
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-19 14:07:08 -0700

    add menu animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open');
+  // Toggle animation slider
+  slideAnim.reversed(!slideAnim.reversed());
 };
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
+// Animation for menu sliding
+let slideAnim = TweenMax.to(menu, 1, { left: '0' }).reverse();
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
