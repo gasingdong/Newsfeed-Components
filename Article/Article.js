1ba86a07921a936da220f7dfd60a3ff772bb1af1
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-19 14:29:10 -0700

    add article expand animations

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,24 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.isOpen = false;
+    this.slideAnim = TweenMax.to(this.domElement, 1, {
+      height: '400px',
+    }).reverse();
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    // With the animation, we don't need to toggle this anymore
+    // this.domElement.classList.toggle('article-open');
+    this.isOpen = !this.isOpen;
+    this.expandButton.textContent = this.isOpen
+      ? 'Click to Close'
+      : 'Click to Expand';
+    this.slideAnim.reversed(!this.slideAnim.reversed());
   }
 }
 
